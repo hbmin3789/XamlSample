54713976b5e5c305257d97fdf3947d1a1694ad86
hbmin3789 <hbmin3789@naver.com>
2021-01-12 12:57:02 +0900

    feat : add bindable class add checkable add treeable

--- CustomControls/Common/Checkable.cs
+++ CustomControls/Common/Checkable.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CustomControls.Common
 {
+    /// <summary>
+    /// 일반 모델을 체크 할 수 있게 만듬
+    /// </summary>
     public class Checkable : BindableBase
     {
+        protected bool _isChecked = false;
+        public virtual bool IsChecked
+        {
+            get => _isChecked;
+            set => SetProperty(ref _isChecked, value);
+        }
+    }
+
+    public class CheckableTreeBase : Checkable
+    {
+        protected ObservableCollection<CheckableTreeBase> _childItems = new ObservableCollection<CheckableTreeBase>();
+        public virtual ObservableCollection<CheckableTreeBase> ChildItems
+        {
+            get => _childItems;
+            set => SetProperty(ref _childItems, value);
+        }
+
+        public override bool IsChecked 
+        {
+            get
+            {
+
+                return _isChecked;
+            }
+            set
+            {
+                SetProperty(ref _isChecked, value);
+            }
+        }
+
+        protected bool GetChildCheck()
+        {
+            if(ChildItems.Count == 0)
+            {
+                return false;
+            }
+            var checkedList = ChildItems.Where(x => x.IsChecked).ToList();
+            if(checkedList.Count == 0)
+            {
+                return null;
+            }
+            return checkedList.Count == ChildItems.Count;
+        }
     }
 }
--- CustomControls/Common/Treeable.cs
+++ CustomControls/Common/Treeable.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CustomControls.Common
 {
-    public class Treeable
+    /// <summary>
+    /// 일반 모델을 트리에 바인딩 할 수 있게 만듬
+    /// </summary>
+    public class Treeable : BindableBase
     {
+        protected ObservableCollection<Treeable> _childItems = new ObservableCollection<Treeable>();
+        public virtual ObservableCollection<Treeable> ChildItems
+        {
+            get => _childItems;
+            set => SetProperty(ref _childItems, value);
+        }
     }
 }
