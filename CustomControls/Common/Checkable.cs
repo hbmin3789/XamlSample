5a7ec9182c3259bb3b57d457e3361b2ae6dc4f29
hbmin3789 <hbmin3789@naver.com>
2021-01-12 12:38:45 +0900

    chore : add prism library

---
+++ CustomControls/Common/Checkable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomControls.Common
+{
+    public class Checkable : BindableBase
+    {
+    }
+}
